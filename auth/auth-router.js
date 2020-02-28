9fd25f450312300fc0bda1e130de22b8f5d07ed6
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-28 11:27:02 -0500

    login endpoint working and had function to generate token working as well

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,6 +4,8 @@ const bc = require("bcryptjs");
 
 const Auth = require("./auth-models.js");
 
+const { jwtSecret } = require("../api/config/secrets.js");
+
 router.post("/register", async (req, res) => {
   // implement registration
   let { username, password } = req.body;
@@ -12,11 +14,9 @@ router.post("/register", async (req, res) => {
     try {
       bc.hash(password, 10, async (err, hash) => {
         if (err) {
-          res
-            .status(500)
-            .json({
-              error: "Sorry. We're having problems. Please try again later"
-            });
+          res.status(500).json({
+            error: "Sorry. We're having problems. Please try again later"
+          });
         } else {
           password = hash;
           const user = await Auth.add({ username, password });
@@ -34,8 +34,38 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+  if (username && password) {
+    try {
+      const user = await Auth.getBy({ username });
+
+      if (user && bc.compareSync(password, user.password)) {
+        const token = signInToken(user);
+        res.status(200).json({ token, id: user.id });
+      } else {
+        res.status(404).json({ error: "No user with that username was found" });
+      }
+    } catch (err) {
+      res.status(404).json({ error: "User can't be found" });
+    }
+  } else {
+    res.status(400).json({ error: "Please provide username and password!" });
+  }
 });
+
+function signInToken(user) {
+  const payload = {
+    userId: user.id
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
